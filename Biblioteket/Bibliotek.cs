0238c0303db0c7d247b6d607f77497f722ebfac1
Jonasthepanda67 <jonas.petersen@danfoss.com>
2023-12-12 15:02:00 +0100

    c3

--- Biblioteket/Bibliotek.cs
+++ Biblioteket/Bibliotek.cs
@@ -10,19 +10,23 @@ namespace Biblioteket
     public class Bibliotek
     {
         string BiblioteksNavn;
-        List<Laaner> laanere = new List<Laaner>();
+        List<Laaner> laanere;
         public Bibliotek(string n) {
             BiblioteksNavn = n;
         }
         public string HentBibliotek() {
             return "Velkommen til " + BiblioteksNavn + " - datoen idag er: " + DateTime.Now;
         }
-        public void Opretlaaner(int lNummer, string navn) {
+        public string Opretlaaner(int lNummer, string navn) {
             Laaner nyLaaner = new Laaner(lNummer, navn);
             laanere.Add(nyLaaner);
+            return "Låner med nummeret: " + lNummer + " og navnet: " + navn + " er nu blevet oprettet";
         }
         public string HentLaaner(Laaner laaner1) {
             return "Lånernummer: " + laaner1.laanerNummer + " - Navn: " + laaner1.navn + " er låner hos: " + BiblioteksNavn;
+        }
+        public void HentAlleLaanere(){
+
         }
     }
 }
